2c6e0a7f78d99213791f4db50f701ae256eeff94
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-08-15 07:39:57 +0530

    Added: Updated Binary Search Program

--- src/com/bridgelabz/BinarySearch.java
+++ src/com/bridgelabz/BinarySearch.java
@@ -4,40 +4,49 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class BinarySearch {
-	
-	public static void main(String[] args) {
-		
-		 Scanner sc = new Scanner(System.in);
-		   int i;  
 
-		   System.out.println("Enter number of words you wish to input: ");
-		   int n=sc.nextInt();
-
-		   String[] words= new String[n];
-
-		   System.out.println("Enter the words");
-		   for ( i = 0; i < words.length; i++)
-		   {
-		       System.out.print("WORD " + (i + 1) + ": ");
-		       words[i] = sc.next();
-		   }
-
-		   System.out.println("enter the word you want to search for");
-		   String word=sc.next();
-
-		   Arrays.sort(words);
-
-
-		   Arrays.binarySearch(words,word);
-		  
-		   for ( i = 0; i < n; i++) {
-		  
-		   if( word.equals(words[i])) 
-		   System.out.println(word + " found!");
-		   else
-			   System.out.println("not found");
-		   }
-	}
-		
-
+	 static int binarySearch(String[] words, String word)
+	    {
+	        int left = 0, right = words.length - 1;
+	        while (left <= right) {
+	            int mid = left + (right - left) / 2;
+	            //System.out.println(mid);
+	            // it compares with words in array
+	            int res = word.compareTo(words[mid]);
+	           // System.out.println(words[mid]);
+	            // Check if x is present at mid
+	            if (res == 0)
+	                return mid;
+	 
+	            // If x greater, ignore left half
+	            if (res > 0)
+	                left = mid + 1;
+	 	           
+	            else
+	                right = mid - 1;
+	        }
+	 
+	        return -1;
+	    }
+	 
+	    // Driver method to test above
+	    public static void main(String []args)
+	    {
+	    	
+	    	Scanner sc = new Scanner(System.in);
+	    	
+	    	 String[] words = { "java", "C", "C++", "Javascript"};
+	    	 System.out.println("Enter the word you want to search for :");
+	         String word = sc.next();
+	         
+	         Arrays.sort(words);
+	         
+	         int result = binarySearch(words, word);
+	  
+	         if (result == -1)
+	             System.out.println("Element not present");
+	         else
+	             System.out.println("Element found at "
+	                               + "index " + result);
+	     }
 }
--- src/com/bridgelabz/Permutations.java
+++ src/com/bridgelabz/Permutations.java
@@ -1,6 +1,74 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.List;
 
+public class Permutations {
+
+	public static void Iterative(String str) {
+		
+		if (str == null || str.length() == 0) {
+			return;
+		}
+
+		List<String> partial = new ArrayList<>();
+
+		partial.add(String.valueOf(str.charAt(0)));
+
+		for (int i = 1; i < str.length(); i++) // i=1<3 condition is true. so it is iterating upto it becomes false
+		{
+
+			for (int j = partial.size() - 1; j >= 0; j--) // j=6-1=5, 5>0, condition true
+			{
+
+				String s = partial.remove(j);
+
+				for (int k = 0; k <= s.length(); k++) {
+
+					partial.add(s.substring(0, k) + str.charAt(i) + s.substring(k));
+
+				}
+			}
+		}
+		
+		System.out.println("partial: " + partial);
+
+	}
+
+	private static void Recursive(String candidate, String remaining) {
+
+		if (remaining == null) {
+			return;
+		}
+
+		if (remaining.length() == 0) {
+			System.out.println(candidate);
+			
+		}
+
+		for (int i = 0; i < remaining.length(); i++) {
+			String newCandidate = candidate + remaining.charAt(i);
+			System.out.println("n: " + newCandidate);
+			System.out.println("c: " + candidate);
+			String newRemaining = remaining.substring(0, i) + remaining.substring(i + 1);
+
+			Recursive(newCandidate, newRemaining);
+		}
+	}
+
+	public static void main(String[] args) {
+		String str = "ABC";
+		String str1 = "ABC";
+		
+		String[] arr = new String[3];
+		
+		Iterative(str);
+		Recursive("", str1);
+		
+		
+		
+	}			 
+		 }
 		
 	
 
