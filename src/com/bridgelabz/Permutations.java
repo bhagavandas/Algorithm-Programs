e6a3b6de12a89fe9d4d6f83f1cf817dcd4832141
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-08-15 08:42:34 +0530

    Added: permutations of a String using iterative method and

--- src/com/bridgelabz/Permutations.java
+++ src/com/bridgelabz/Permutations.java
@@ -1,12 +1,13 @@
 package com.bridgelabz;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Permutations {
 
 	public static void Iterative(String str) {
-		
+
 		if (str == null || str.length() == 0) {
 			return;
 		}
@@ -30,7 +31,7 @@ public class Permutations {
 				}
 			}
 		}
-		
+
 		System.out.println("partial: " + partial);
 
 	}
@@ -43,32 +44,58 @@ public class Permutations {
 
 		if (remaining.length() == 0) {
 			System.out.println(candidate);
-			
+
 		}
 
 		for (int i = 0; i < remaining.length(); i++) {
 			String newCandidate = candidate + remaining.charAt(i);
-			System.out.println("n: " + newCandidate);
-			System.out.println("c: " + candidate);
+			//System.out.println("n: " + newCandidate);
+			//System.out.println("c: " + candidate);
 			String newRemaining = remaining.substring(0, i) + remaining.substring(i + 1);
 
 			Recursive(newCandidate, newRemaining);
+
 		}
+
+	}
+
+	static boolean arePermutation(String str, String str1) {
+		// Get lengths of both strings
+		int n1 = str.length();
+		int n2 = str1.length();
+
+		// If length of both strings is not same,
+		// then they cannot be Permutation
+		if (n1 != n2)
+			return false;
+		char ch1[] = str.toCharArray();
+		char ch2[] = str1.toCharArray();
+
+		// Sort both strings
+		Arrays.sort(ch1);
+		Arrays.sort(ch2);
+
+		// Compare sorted strings
+		for (int i = 0; i < n1; i++)
+			if (ch1[i] != ch2[i])
+				return false;
+
+		return true;
 	}
 
 	public static void main(String[] args) {
 		String str = "ABC";
-		String str1 = "ABC";
-		
-		String[] arr = new String[3];
-		
+		String str1 = "AFC";
+
+		if (arePermutation(str, str1))
+			System.out.println("The arrays returned by two string functions are equal.");
+		else
+			System.out.println("The arrays returned by two string functions are NOT equal.");
+
+		// String[] arr = new String[3];
+
 		Iterative(str);
 		Recursive("", str1);
-		
-		
-		
-	}			 
-		 }
-		
-	
 
+	}
+}
